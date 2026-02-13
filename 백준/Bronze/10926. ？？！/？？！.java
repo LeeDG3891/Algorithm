ecf22bb2ac6f427f38a19214a1b2521b9d843c94
LeeDoGyeong <115414086+LeeDG3891@users.noreply.github.com>
2026-02-13 19:38:59 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		String a;
+		Scanner scan = new Scanner(System.in);
+		
+		a = scan.next();
+		
+		System.out.print(a + "??!");
+	}
+}

6c537c78d45e709b9ca304c45448e5ab53b30260
LeeDoGyeong <115414086+LeeDG3891@users.noreply.github.com>
2026-02-16 02:14:34 +0900

    [Bronze IV] Title: 영수증, Time: 176 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        int n = scan.nextInt();
+        int sum = 0;
+
+        for(int i=0; i<n; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            sum += a * b;
+        }
+
+        if (x == sum)   System.out.println("Yes");
+        else System.out.println("No");
+    }
+}

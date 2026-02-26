96414571fca7f7e4e0559072c9a0be8f65701e20
LeeDoGyeong <115414086+LeeDG3891@users.noreply.github.com>
2026-02-26 23:58:49 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count1 = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            int count2 = 0;
+
+            for (int j=1; j<a; j++) {
+                if (a % j == 0) count2++;
+            }
+            if (count2 == 1) count1++;
+        }
+        System.out.println(count1);
+
+    }
+}

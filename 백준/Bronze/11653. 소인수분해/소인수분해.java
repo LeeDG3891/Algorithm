a815db122da40aa255a5e40bb20586929a22c5f5
LeeDoGyeong <115414086+LeeDG3891@users.noreply.github.com>
2026-02-27 23:27:44 +0900

    [Bronze I] Title: 소인수분해, Time: 140 ms, Memory: 14692 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) return;
+
+        for (int i=2; i<n; i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1)  System.out.println(n);
+    }
+}

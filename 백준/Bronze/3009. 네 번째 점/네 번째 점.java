8fcc9f0e2209642a41d294460e470f7d8aa20fc9
LeeDoGyeong <115414086+LeeDG3891@users.noreply.github.com>
2026-02-28 00:04:49 +0900

    [Bronze III] Title: 네 번째 점, Time: 124 ms, Memory: 15956 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = 0;
+        int y = 0;
+
+        for (int i=0; i<3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x ^= Integer.parseInt(st.nextToken());
+            y ^= Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(x + " " + y);
+
+
+    }
+}

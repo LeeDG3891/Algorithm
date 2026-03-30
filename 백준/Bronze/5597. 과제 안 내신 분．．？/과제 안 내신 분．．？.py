3b915c214e5234e47aa3cbc584244eef50c2934d
LeeDoGyeong <115414086+LeeDG3891@users.noreply.github.com>
2026-03-30 12:59:10 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+arr = [0] * 31
+
+for _ in range (28):
+    n = int(input())
+    arr[n] = 1
+
+for i in range(1, 31):
+    if (arr[i] == 0):
+        print(i)
